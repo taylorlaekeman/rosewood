75af339776a840489094d1cc83cd55312e84a057
taylor <taylor.laekeman@gmail.com>
2026-01-25 14:36:20 -0500

    add question count

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,7 @@ function App() {
     enabledFrets,
   });
   const [selectedNote, setSelectedNote] = useState<Note | undefined>();
+  const [count, setCount] = useState<number>(1);
   return (
     <>
       <h1>Rosewood</h1>
@@ -84,32 +85,43 @@ function App() {
       >
         Practice
       </button>
-      <Fretboard highlights={[{ fret, guitarString }]} />
-      <fieldset>
-        {NOTES.map((note) => (
-          <div key={note}>
-            <input
-              checked={note === selectedNote}
-              id={note}
-              name="note"
-              onChange={() => setSelectedNote(note)}
-              type="radio"
-              value={note}
-            />
-            <label htmlFor={note}>{note}</label>
-          </div>
-        ))}
-      </fieldset>
-      {selectedNote === note && <p>correct!</p>}
-      {selectedNote && selectedNote !== note && <p>try again</p>}
-      <button
-        onClick={() => {
-          setSelectedNote(undefined);
-          getNewNote();
-        }}
-      >
-        Next
-      </button>
+      {count > 10 ? (
+        <>
+          <p>done!</p>
+          <button onClick={() => setCount(1)}>restart</button>
+        </>
+      ) : (
+        <>
+          <p>{`${count} / 10`}</p>
+          <Fretboard highlights={[{ fret, guitarString }]} />
+          <fieldset>
+            {NOTES.map((note) => (
+              <div key={note}>
+                <input
+                  checked={note === selectedNote}
+                  id={note}
+                  name="note"
+                  onChange={() => setSelectedNote(note)}
+                  type="radio"
+                  value={note}
+                />
+                <label htmlFor={note}>{note}</label>
+              </div>
+            ))}
+          </fieldset>
+          {selectedNote === note && <p>correct!</p>}
+          {selectedNote && selectedNote !== note && <p>try again</p>}
+          <button
+            onClick={() => {
+              setSelectedNote(undefined);
+              setCount((currentCount) => currentCount + 1);
+              getNewNote();
+            }}
+          >
+            Next
+          </button>
+        </>
+      )}
     </>
   );
 }
