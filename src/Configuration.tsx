fa0cbf49280670ace1e9b849ae0b4473314b709d
taylor <taylor.laekeman@gmail.com>
2026-01-25 22:39:34 -0500

    add configuration presets and extract custom configuration

--- src/Configuration.tsx
+++ src/Configuration.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { CustomConfiguration } from './CustomConfiguration';
 import { FRETS, type Fret } from './frets';
 import { GUITAR_STRINGS, type GuitarString } from './guitarStrings';
 
@@ -12,107 +13,73 @@ export function Configuration({
     enabledStrings: GuitarString[];
   }) => void;
 }) {
-  const [stringConfiguration, setStringConfiguration] =
-    useState<StringConfiguration>(ALL_STRINGS_ENABLED);
-  const [fretConfiguration, setFretConfiguration] =
-    useState<FretConfiguration>(ALL_FRETS_ENABLED);
-  const enabledStrings = Object.entries(stringConfiguration)
-    .filter(([_, enabled]) => enabled)
-    .map(([guitarString]) => guitarString as GuitarString);
-  const enabledFrets = Object.entries(fretConfiguration)
-    .filter(([_, enabled]) => enabled)
-    .map(([fret]) => parseInt(fret, 10) as Fret);
+  const [isCustomConfiguration, setIsCustomConfiguration] =
+    useState<boolean>(false);
+  if (isCustomConfiguration)
+    return (
+      <CustomConfiguration
+        onCancel={() => setIsCustomConfiguration(false)}
+        onStart={onConfigure}
+      />
+    );
   return (
     <>
-      <div>
-        <p>Strings</p>
-        {GUITAR_STRINGS.map((guitarString) => (
-          <div key={guitarString}>
-            <input
-              checked={stringConfiguration[guitarString]}
-              id={guitarString}
-              onChange={() => {
-                setStringConfiguration((currentStringConfiguration) => ({
-                  ...currentStringConfiguration,
-                  [guitarString]: !currentStringConfiguration[guitarString],
-                }));
-              }}
-              name={guitarString}
-              type="checkbox"
-            />
-            <label htmlFor={guitarString}>{guitarString}</label>
-          </div>
-        ))}
-        {enabledStrings.length === 0 && (
-          <p>Please select at least one string</p>
-        )}
-      </div>
-      <div>
-        <p>Frets</p>
-        {FRETS.map((fret) => {
-          const fretString = fret.toString(10);
-          return (
-            <div key={fretString}>
-              <input
-                checked={fretConfiguration[fret]}
-                id={fretString}
-                onChange={() => {
-                  setFretConfiguration((currentFretConfiguration) => ({
-                    ...currentFretConfiguration,
-                    [fret]: !currentFretConfiguration[fret],
-                  }));
-                }}
-                name={fretString}
-                type="checkbox"
-              />
-              <label htmlFor={fretString}>{fret}</label>
-            </div>
-          );
-        })}
-        {enabledFrets.length === 0 && <p>Please select at least one fret</p>}
-      </div>
-      {enabledStrings.length === 1 && enabledFrets.length === 1 && (
-        <p>Please select at least two notes</p>
-      )}
-      <button
-        disabled={
-          enabledStrings.length === 0 ||
-          enabledFrets.length === 0 ||
-          (enabledStrings.length === 1 && enabledFrets.length === 1)
-        }
-        onClick={() => onConfigure({ enabledFrets, enabledStrings })}
-      >
-        Practice
+      {PRESET_OPTIONS.map((option) => (
+        <button
+          key={option.id}
+          onClick={() => {
+            onConfigure({
+              enabledFrets: option.enabledFrets,
+              enabledStrings: option.enabledStrings,
+            });
+          }}
+        >
+          {option.label}
+        </button>
+      ))}
+      <button onClick={() => setIsCustomConfiguration(true)}>
+        Custom Configuration
       </button>
     </>
   );
 }
 
-type StringConfiguration = Record<GuitarString, boolean>;
+const PRESET_OPTIONS: PresetOption[] = [
+  {
+    id: 'full',
+    label: 'Full Fretboard',
+    enabledFrets: FRETS,
+    enabledStrings: GUITAR_STRINGS,
+  },
+  {
+    id: 'chords',
+    label: 'Open Chords',
+    enabledFrets: [0, 1, 2, 3],
+    enabledStrings: GUITAR_STRINGS,
+  },
+  {
+    id: 'low',
+    label: 'Low Strings',
+    enabledFrets: FRETS,
+    enabledStrings: ['E', 'A'],
+  },
+  {
+    id: 'middle',
+    label: 'Middle Strings',
+    enabledFrets: FRETS,
+    enabledStrings: ['D', 'G'],
+  },
+  {
+    id: 'high',
+    label: 'High Strings',
+    enabledFrets: FRETS,
+    enabledStrings: ['B', 'e'],
+  },
+];
 
-const ALL_STRINGS_ENABLED: StringConfiguration = {
-  E: true,
-  A: true,
-  D: true,
-  G: true,
-  B: true,
-  e: true,
-};
-
-type FretConfiguration = Record<Fret, boolean>;
-
-const ALL_FRETS_ENABLED: FretConfiguration = {
-  0: true,
-  1: true,
-  2: true,
-  3: true,
-  4: true,
-  5: true,
-  6: true,
-  7: true,
-  8: true,
-  9: true,
-  10: true,
-  11: true,
-  12: true,
-};
+interface PresetOption {
+  enabledFrets: Fret[];
+  enabledStrings: GuitarString[];
+  id: string;
+  label: string;
+}
