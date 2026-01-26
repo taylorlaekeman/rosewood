75b377787a8613a974fbd14db26668d6b75bdf5e
taylor <taylor.laekeman@gmail.com>
2026-01-25 22:13:23 -0500

    extract configuration to separate file

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,17 @@
 import { useState } from 'react';
 import './App.css';
+import { Configuration } from './Configuration';
 import { Fretboard } from './Fretboard';
-import { FRETS, type Fret } from './frets';
-import { GUITAR_STRINGS, type GuitarString } from './guitarStrings';
-import { useRandomNote } from './useRandomNote';
+import { type Fret } from './frets';
+import { type GuitarString } from './guitarStrings';
 import { NOTES, type Note } from './notes';
+import { useRandomNote } from './useRandomNote';
 
 function App() {
-  const [stringConfiguration, setStringConfiguration] =
-    useState<Record<GuitarString, boolean>>(ALL_STRINGS_ENABLED);
-  const [fretConfiguration, setFretConfiguration] =
-    useState<Record<Fret, boolean>>(ALL_FRETS_ENABLED);
-  const enabledStrings = Object.entries(stringConfiguration)
-    .filter(([_, enabled]) => enabled)
-    .map(([guitarString]) => guitarString as GuitarString);
-  const enabledFrets = Object.entries(fretConfiguration)
-    .filter(([_, enabled]) => enabled)
-    .map(([fret]) => parseInt(fret, 10) as Fret);
+  const [enabledStrings, setEnabledStrings] = useState<
+    GuitarString[] | undefined
+  >();
+  const [enabledFrets, setEnabledFrets] = useState<Fret[] | undefined>();
   const { fret, getNewNote, guitarString, note } = useRandomNote({
     enabledStrings,
     enabledFrets,
@@ -26,65 +21,12 @@ function App() {
   return (
     <>
       <h1>Rosewood</h1>
-      <div>
-        <p>Strings</p>
-        {GUITAR_STRINGS.map((guitarString) => (
-          <div key={guitarString}>
-            <input
-              checked={stringConfiguration[guitarString]}
-              id={guitarString}
-              onChange={() => {
-                setStringConfiguration((currentStringConfiguration) => ({
-                  ...currentStringConfiguration,
-                  [guitarString]: !currentStringConfiguration[guitarString],
-                }));
-              }}
-              name={guitarString}
-              type="checkbox"
-            />
-            <label htmlFor={guitarString}>{guitarString}</label>
-          </div>
-        ))}
-        {enabledStrings.length === 0 && (
-          <p>Please select at least one string</p>
-        )}
-      </div>
-      <div>
-        <p>Frets</p>
-        {FRETS.map((fret) => {
-          const fretString = fret.toString(10);
-          return (
-            <div key={fretString}>
-              <input
-                checked={fretConfiguration[fret]}
-                id={fretString}
-                onChange={() => {
-                  setFretConfiguration((currentFretConfiguration) => ({
-                    ...currentFretConfiguration,
-                    [fret]: !currentFretConfiguration[fret],
-                  }));
-                }}
-                name={fretString}
-                type="checkbox"
-              />
-              <label htmlFor={fretString}>{fret}</label>
-            </div>
-          );
-        })}
-        {enabledFrets.length === 0 && <p>Please select at least one fret</p>}
-      </div>
-      {enabledStrings.length === 1 && enabledFrets.length === 1 && (
-        <p>Please select at least two notes</p>
-      )}
-      <button
-        disabled={
-          enabledStrings.length === 0 ||
-          enabledFrets.length === 0 ||
-          (enabledStrings.length === 1 && enabledFrets.length === 1)
-        }
-      >
-        Practice
-      </button>
+      <Configuration
+        onConfigure={({ enabledFrets, enabledStrings }) => {
+          setEnabledFrets(enabledFrets);
+          setEnabledStrings(enabledStrings);
+        }}
+      />
       {count > 10 ? (
         <>
           <p>done!</p>
@@ -125,30 +67,5 @@ function App() {
     </>
   );
 }
-
-const ALL_STRINGS_ENABLED: Record<GuitarString, boolean> = {
-  E: true,
-  A: true,
-  D: true,
-  G: true,
-  B: true,
-  e: true,
-};
-
-const ALL_FRETS_ENABLED: Record<Fret, boolean> = {
-  0: true,
-  1: true,
-  2: true,
-  3: true,
-  4: true,
-  5: true,
-  6: true,
-  7: true,
-  8: true,
-  9: true,
-  10: true,
-  11: true,
-  12: true,
-};
 
 export default App;
